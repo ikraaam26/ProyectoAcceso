ce72d4b2c504363dcfbd3b0a4614c03cfa9e6fb3
dani <>
2023-11-28 14:04:42 +0100

    Cambio main

---
+++ src/gestioncursos/GestionCursos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestioncursos;
+
+/**
+ *
+ * @author Diurno
+ */
+public class GestionCursos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Estoy cambiando");
+    }
+    
+}
