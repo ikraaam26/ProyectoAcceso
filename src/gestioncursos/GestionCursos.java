b6f4fa38f0d69eab5c9dd6d5993d874471b5436c
Diurno <Diurno@C06PC27.informatica.comercio>
2023-12-04 14:11:22 +0100

    conexion

--- src/gestioncursos/GestionCursos.java
+++ src/gestioncursos/GestionCursos.java
@@ -4,18 +4,33 @@
  */
 package gestioncursos;
 
+import gestioncursos.Modelo.ConexionSQLServer;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
 /**
  *
  * @author Diurno
  */
 public class GestionCursos {
 
-    /**
+  /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Estoy cambiando");
-        String nombre = " ";
+        ConexionSQLServer con = new ConexionSQLServer();
+        
+        try (Connection c = con.getConnection()) {
+            DatabaseMetaData informacion = c.getMetaData();
+            String nombre = informacion.getDatabaseProductName();
+            String driver = informacion.getDriverName();
+            String url = informacion.getURL();
+            String usuario = informacion.getUserName();
+            
+            System.out.println(nombre + " " + driver + " " + url + " " + usuario);
+        } catch (SQLException e) {
+            System.err.println("Error en la consulta: " + e.getMessage());
+        }
     }
-    
 }
--- src/gestioncursos/Modelo/ConexionSQLServer.java
+++ src/gestioncursos/Modelo/ConexionSQLServer.java
@@ -4,10 +4,25 @@
  */
 package gestioncursos.Modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Diurno
  */
 public class ConexionSQLServer {
-    
+        private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=GestionCursos";
+    private static final String USUARIO = "C06PC27\\Diurno";
+    private static final String CONTRASENA = "";
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Error al cargar el controlador JDBC: " + e.getMessage());
+        }
+    }
 }
