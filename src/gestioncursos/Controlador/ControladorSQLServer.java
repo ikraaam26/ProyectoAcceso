1527461686acdbd504c3ec876e5a3702b1f9a21a
Diurno <Diurno@C06PC28.informatica.comercio>
2023-12-18 12:32:49 +0100

    Controlador aactualizado

--- src/gestioncursos/Controlador/ControladorSQLServer.java
+++ src/gestioncursos/Controlador/ControladorSQLServer.java
@@ -4,10 +4,62 @@
  */
 package gestioncursos.Controlador;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 /**
  *
  * @author Diurno
  */
-public class ControladorSQLServer {
+public class ControladorSQLServer implements ActionListener, KeyListener{
+    
+    gestioncursos.Vista.Vista view;
+    gestioncursos.Modelo.AccesoDAO modelo;
+
+    public ControladorSQLServer(gestioncursos.Vista.Vista view, gestioncursos.Modelo.AccesoDAO modelo) {
+        this.view = view;
+        this.modelo = modelo;
+        añadirKeyListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+         /* String action= e.getActionCommand();
+          switch (action) {
+            case "Consultar":
+                String localidad= view.txtLocalidad.getText();
+                model.queryTable("departamentos");
+                break;
+            default:
+                throw new AssertionError();
+        }*/
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void añadirAcionListener(ActionListener lis) {
+
+    }
+
+    private void añadirKeyListener(KeyListener kl) {
+    
+    }
     
 }
+
+
